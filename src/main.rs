81dc7bb58a1b27f74a5797f2ea7f5731f45eb7d9
Sulaiman Gafoor <5022721+suli-g@users.noreply.github.com>
2024-08-26 17:33:10 +0200

    Create a dynamic Input element to handle inputs.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
+use leptos::*;
+
+/// Renders an input element with a given `title` and `setter` 
+#[component]
+fn Input(
+    setter: WriteSignal<String>,
+) -> impl IntoView {
+    
+    view! {
+        <input type="text"
+            // event_target_value is similar to e.target.value in JS:
+            on:input=move |ev| {
+                setter(event_target_value(&ev))
+            }
+
+            // `prop:` updates a dom property rather than an attribute.
+            />
+        }
+}
+
+/// Renders the main application.
+#[component]
+fn App() -> impl IntoView {
+    let (name, set_name) = create_signal("Controller".to_string());
+    view! {
+        <Input setter=set_name />
+        <p>{name}</p>
+        }
+}
+
 fn main() {
-    console-error_panic_hook::set_once();
-    println!("Hello, world!");
+    console_error_panic_hook::set_once();
+    mount_to_body(App);
 }
